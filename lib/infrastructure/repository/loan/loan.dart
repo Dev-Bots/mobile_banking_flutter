803ab8d762e5b52def5ae2c04afe1d11f4ee4667
Tinsae <itistinsae@gmail.com>
2021-08-27 22:18:04 +0300

    loan

---
+++ lib/infrastructure/repository/loan/loan.dart
@@ -0,0 +1,1 @@
+export 'loanRepository.dart';
---
+++ lib/infrastructure/repository/loan/loanRepository.dart
@@ -0,0 +1,27 @@
+import 'package:mobile_banking/domain/models/models.dart';
+import 'package:mobile_banking/domain/models/TransactionHistory.dart';
+import 'package:mobile_banking/insfrastructure/data_provider/loan/loanProvider.dart';
+
+class LoanRepository {
+  final LoanDataProvider dataProvider;
+
+  LoanRepository({required this.dataProvider});
+
+  Future getLoanInfo() async {
+    return await dataProvider.getLoanInfo();
+  }
+
+  Future takeLoan(double amount) async {
+    return await dataProvider.takeLoan(amount);
+  }
+
+  Future loanTopUp(double amount) async {
+    return await dataProvider.loanTopUp(amount);
+  }
+
+  Future loanPayInFull() async {
+    return await dataProvider.loanPayInFull();
+  }
+}
+
+// class NetworkError extends Error {}
