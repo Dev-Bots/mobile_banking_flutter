78de6c5423dc30ad7a57a934bc05a87684050306
kevin-shitaye <it.kevin.shitaye@gmail.com>
2021-09-01 19:37:11 +0300

    finished the auth bloc files

--- lib/application/bloc/AuthBloc/auth_event.dart
+++ lib/application/bloc/AuthBloc/auth_event.dart
@@ -6,3 +6,83 @@ abstract class AuthEvent extends Equatable {
   @override
   List<Object> get props => [];
 }
+
+class GetMyAccount extends AuthEvent {
+  @override
+  List<Object> get props => [];
+}
+
+class GetAccount extends AuthEvent {
+  final accountNumber;
+
+  GetAccount({required this.accountNumber});
+
+  @override
+  List<Object> get props => [accountNumber];
+}
+
+class RegisterAgent extends AuthEvent {
+  final Agent agent;
+
+  RegisterAgent({required this.agent});
+
+  @override
+  List<Object> get props => [agent];
+}
+
+class RegisterClient extends AuthEvent {
+  final Client client;
+
+  RegisterClient({required this.client});
+
+  @override
+  List<Object> get props => [client];
+}
+
+class ChangePassword extends AuthEvent {
+  final String password;
+
+  ChangePassword({required this.password});
+
+  @override
+  List<Object> get props => [password];
+}
+
+class SaveAccount extends AuthEvent {
+  final accountNumber;
+
+  SaveAccount({required this.accountNumber});
+
+  @override
+  List<Object> get props => [accountNumber];
+}
+
+class RemoveFromSaved extends AuthEvent {
+  final accountNumber;
+
+  RemoveFromSaved({required this.accountNumber});
+
+  @override
+  List<Object> get props => [accountNumber];
+}
+
+class BlockUnblock extends AuthEvent {
+  final accountNumber;
+
+  BlockUnblock({required this.accountNumber});
+
+  @override
+  List<Object> get props => [accountNumber];
+}
+
+class ChangeAccountType extends AuthEvent {
+  final int accountType;
+  final accountNumber;
+
+  ChangeAccountType({required this.accountNumber, required this.accountType});
+
+  @override
+  List<Object> get props => [accountNumber, accountType];
+}
+
+class GetGeneralReport extends AuthEvent {}
