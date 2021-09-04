4e40f5fc0ac82cf33d44ea66faa128c3037609a9
kevin-shitaye <it.kevin.shitaye@gmail.com>
2021-09-04 14:33:00 +0300

    reusable widget bankname and avatar

---
+++ lib/presentation/widgets/bank_name.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+
+class BankName extends StatelessWidget {
+  const BankName({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      // Bank Name and Logo
+      margin: EdgeInsets.only(top: 20),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Icon(
+            Icons.account_balance,
+            color: Colors.blue[800],
+          ),
+          SizedBox(width: 10),
+          Text(
+            'Dominion Bank Corporation',
+            style: TextStyle(
+                fontWeight: FontWeight.bold,
+                color: Colors.blue[800],
+                fontSize: 25),
+          ),
+        ],
+      ),
+    );
+  }
+}
