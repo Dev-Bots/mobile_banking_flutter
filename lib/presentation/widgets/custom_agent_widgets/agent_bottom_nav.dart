640dcbf1ca14ea22880d3e80e7b8b80b2114270b
Meron Mesay <it.meron.mesay@gmail.com>
2021-09-09 08:27:45 +0300

    created an agent widget

---
+++ lib/presentation/widgets/custom_agent_widgets/agent_bottom_nav.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+
+class AgentBottomNavigation extends StatelessWidget {
+  const AgentBottomNavigation({this.selectedIndex = 0, required this.onTap});
+
+  final int selectedIndex;
+  final void Function(int) onTap;
+
+  @override
+  Widget build(BuildContext context) {
+    return BottomNavigationBar(
+      items: const <BottomNavigationBarItem>[
+        BottomNavigationBarItem(
+          icon: Icon(Icons.dashboard),
+          label: 'Dashboard',
+        ),
+        BottomNavigationBarItem(
+          icon: Icon(Icons.person),
+          label: 'Profile',
+        ),
+        // BottomNavigationBarItem(
+        //   icon: Icon(Icons.settings),
+        //   label: 'Settings',
+        // ),
+        // BottomNavigationBarItem(
+        //   icon: Icon(Icons.person),
+        //   label: 'Profile',
+        // ),
+      ],
+      elevation: 10,
+      currentIndex: selectedIndex,
+      selectedItemColor: Colors.lightGreenAccent,
+      unselectedItemColor: Colors.cyanAccent,
+      onTap: onTap,
+    );
+  }
+}
